b43d81a8cea36efb8317e63394730e6dc3cbabe2
Uniliva Alves Pereira <uniliva-ap@hotmail.com>
2018-01-03 22:45:56 -0200

    craiando classe negociação e configurando typeScript

---
+++ TypeScript paarte 1/app/js/app.js
@@ -0,0 +1,2 @@
+const negociacao = new Negociacao(new Date(),4,2);
+console.log(negociacao.volume);
---
+++ TypeScript paarte 1/app/js/models/Negociacao.js
@@ -0,0 +1,13 @@
+class Negociacao {
+    //criando o construtor da classe Negociação
+    constructor(data, quantidade, valor) {
+        this._data = data;
+        this._quatidade = quantidade;
+        this._valor = valor;
+    }
+    //criando getters
+    get data() { return this._data }
+    get quantidade() { return this._quatidade }
+    get valor() { return this._valor }
+    get volume() { return this._quatidade * this._valor }
+}
