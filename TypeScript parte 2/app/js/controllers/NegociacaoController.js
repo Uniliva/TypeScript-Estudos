1acc37903d58476207ba32cd5ce2ebb5dccd2e26
Uniliva Alves Pereira <uniliva-ap@hotmail.com>
2018-01-09 15:28:26 -0200

    Configurando um servidor local - para executarmos a aplicação

--- TypeScript parte 2/app/js/controllers/NegociacaoController.js
+++ TypeScript parte 2/app/js/controllers/NegociacaoController.js
@@ -17,9 +17,9 @@ class NegociacaoController {
         this._limpaCampos();
     }
     _limpaCampos() {
-        this._inputData.value = "0";
-        this._inputQuantidade.value = "1";
-        this._inputValor.value = "0";
+        this._inputData.text("1");
+        this._inputQuantidade.text("1");
+        this._inputValor.text(0);
         this._inputData.focus();
     }
 }
