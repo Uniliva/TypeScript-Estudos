dc86168fa3131eb2b934f5f2bbfe2e9b219da596
Uniliva Alves Pereira <uniliva-ap@hotmail.com>
2018-01-11 07:51:29 -0200

    merge
    Signed-off-by: Uniliva Alves Pereira <uniliva-ap@hotmail.com>

--- TypeScript parte 2/app/ts/models/Negociacao.ts
+++ TypeScript parte 2/app/ts/models/Negociacao.ts
@@ -1,10 +1,7 @@
 export class Negociacao {
     //criando o construtor da classe Negociação
-    constructor(private _data:Date, private _quantidade:number, private _valor:number) {
+    constructor(readonly data:Date,  readonly quantidade:number,  readonly valor:number) {
     }
-    //criando getters
-    get data() { return this._data }
-    get quantidade() { return this._quantidade }
-    get valor() { return this._valor }
-    get volume() { return (this._quantidade * this._valor) }
+  
+    get volume() { return (this.quantidade * this.valor) }
 }
